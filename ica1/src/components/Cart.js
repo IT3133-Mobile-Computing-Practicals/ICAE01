e03644e7e1ac3763efa09462d359cbd02216a118
TharinduLakshimal <tharindulakshimal@gmail.com>
2024-12-03 18:40:24 +0530

    feat:add grandtotal function

--- ica1/src/components/Cart.js
+++ ica1/src/components/Cart.js
@@ -1,4 +1,9 @@
 export default function Cart(props) {
+    let grandTotal = 0;
+    for (const item of props.getCart) {
+        grandTotal += item.quantity * item.flowerPrice;
+    }
+
     return (
         <div className="table-container">
             <h4 className="card-title">Cart</h4>
@@ -18,6 +23,10 @@ export default function Cart(props) {
                             <td>{item.quantity*item.flowerPrice}</td>
                         </tr>
                     ))}
+                    <tr>
+                    <td >Grand Total : </td>
+                    <td colSpan={2}>{grandTotal}</td>
+                </tr>
                 </tbody>
             </table>
         </div>
